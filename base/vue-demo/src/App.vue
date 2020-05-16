86964fbdd0784dd1dde48b83df172f62daa074a8
sunmd <sun-ming-da@163.com>
2020-05-16 09:08:59 +0800

    添加v-module实例

--- base/vue-demo/src/App.vue
+++ base/vue-demo/src/App.vue
@@ -4,10 +4,12 @@
             单文件组件,用的时nodejs的npm安装vue@cli,后面设置环境变量之后,就可以用vue create里生成数据了
             里面的主界面在pubulic里面,主入口在main.js里面.之后生成实例为app.js,对应的组件都在component里面
             </h3>
+            <h3> 添加一个v-module的测试.试一下是否好用就可以</h3>
             <ul>
                 <todo-item :del="del" :content="content"></todo-item>
             </ul>
-
+            <h1>{{content}}</h1>
+            <input type="text" v-model="content"/>
             <todo-list>
                 <todo-item v-for="item in list" :del="item.del" :key="item.title" :content="item.title" @delete="handleDelete">
                     <span slot="pre-icon"> --前--</span>
