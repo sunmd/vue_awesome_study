ca81dbe660295ce6a54f5a8a794252a4c483398b
sunmd <sun-ming-da@163.com>
2020-05-18 11:01:50 +0800

    添加新的版本测试data和props之间的刷新机制

---
+++ base/study_reflash_data_props/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <p>
+        <button @click="handleNameChange"> change this.name</button>
+        <button @click="handleInfoChange"> change this.info</button>
+        <button @click="handleListChange"> change this.list</button>
+    </p>
+    <p>
+        <PropsAndData :list="list" :name="name" :info="info" > </PropsAndData>
+    </p>
+  </div>
+</template>
+
+<script>
+import PropsAndData from "./components/PropsAndData.vue"
+let name = "world!"
+let number = 1
+export default {
+    name: 'App',
+    components: {
+        PropsAndData
+    },
+    data() {
+        this.name = name;
+        return {
+            name: this.name,
+            info: {
+                // number: undefined
+                  },
+            list: []
+        };
+    },
+    methods: {
+         handleNameChange() {
+            this.name = name + Date.now()
+            console.log("this.name is change", this.name)
+         },
+         handleInfoChange() {
+            number = number + 1
+            this.info.number = number
+            console.log("this.info.number is change", this.info.number)
+         },
+         handleListChange() {
+            this.list.push(1, 2, 3)
+            console.log("this.list is change", this.list)
+         }
+
+     }
+
+}
+</script>
+
---
+++ base/study_reflash_data_props/src/components/PropsAndData.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>props.info: {{info}} </p>
+        <p>props.name: {{name}} </p>
+        <p>props.list: {{list}} </p>
+        <p>data.a: {{ a }} </p>
+        <p>
+            <button @click="handleDataB">change data.b</button>
+        </p>
+
+    </div> 
+</template>
+
+<script>
+export default {
+    name: "PropsAndData",
+    props: {
+        info: Object,
+        name: String,
+        list: Array
+    }, 
+    data() {
+        return{
+            a: "hello",
+            b: "world!"
+        };
+    },
+    updated() {
+        console.log("当前数据已经刷新, updatud触发了");
+    },
+    methods: {
+         handleDataB() { 
+            this.b = "vue" + Date.now()
+            console.log("change data b:", this.b)
+         }
+    }
+}
+</script>
