19299736348c6505f095f1565db8637e095d9e07
sunmd <sun-ming-da@163.com>
2020-05-22 14:28:22 +0800

    测试Vuex的使用方式

---
+++ ecology/vuex-text/vuex-test-1-base/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+  <h3>state测试</h3>
+  this is count : {{getCount}}<br/>
+  this is count : {{count}}
+  <p><button @click="addCount">count++</button></p>
+  <p><button @click="addNumber({acount:2})">count+2</button></p>
+  <hr>
+  <h3>getters测试</h3>
+  <p>{{doneTodos}} : {{doneTodosCount}} </p>
+  <p>{{getTodosByid(2)}}</p>
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters, mapMutations} from 'vuex'
+import {STORE_MUSTATION} from './store/mutation-types'
+export default {
+    name: 'App',
+    computed: {
+        getCount() {
+            return this.$store.state.count;
+        },
+        ...mapState({count: state => state.count}),
+        ...mapGetters(['doneTodos', 'doneTodosCount', 'getTodosByid'])
+
+    },
+    methods: {
+        ...mapMutations({addCount: STORE_MUSTATION.ADD_COUNT, 
+                        addNumber: STORE_MUSTATION.ADD_NUMBER})
+        
+         /* addCount() { */
+         /*    this.$store.commit(STORE_MUSTATION.ADD_COUNT); */
+         /* } */
+
+
+     }
+}
+</script>
+
+<style>
+</style>
---
+++ ecology/vuex-text/vuex-test-1-base/src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import App from './App.vue'
+import {STORE_MUSTATION} from './store/mutation-types'
+
+Vue.config.productionTip = false
+Vue.use(Vuex)
+const store = new Vuex.Store({
+    state: {
+        count: 0,
+        todos: [{id: 1, text: "1", done: true},
+                {id: 2, text: "2", done: false},
+                {id: 3, text: "3", done: true},
+                {id: 4, text: "4", done: false},
+                {id: 5, text: "5", done: true},
+                {id: 6, text: "6", done: false},
+        ]
+    },
+    mutations: {
+        [STORE_MUSTATION.ADD_COUNT](state) {
+            state.count++;
+        },
+        [STORE_MUSTATION.ADD_NUMBER] : (state, payload) => state.count += payload.acount
+    },
+    getters: {
+        doneTodos: state => state.todos.filter(todo => todo.done ),
+        doneTodosCount: (state, getters) => {
+            return getters.doneTodos.length;
+        },
+        getTodosByid: state => id => state.todos.find(todo => todo.id === id)
+    }
+})
+
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount('#app')
