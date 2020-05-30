1f1dfc5055460106239e415a55f662d33e89445d
sunmd <sun-ming-da@163.com>
2020-05-30 13:34:48 +0800

    添加实战的测试部分,使用ant-design-vue来做实战

---
+++ actualCombat/ant-design-vue-pro-test/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: ["@vue/cli-plugin-babel/preset"],
+  // .babelrc or babel-loader option
+  "plugins": [
+    ["import", {
+      "libraryName": "ant-design-vue", "libraryDirectory":
+        "es", "style": true
+    }] // `style: true` 会加载 less 文件
+  ]
+};
---
+++ actualCombat/ant-design-vue-pro-test/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <a-button>按钮</a-button>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+</style>
