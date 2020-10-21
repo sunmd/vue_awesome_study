8e1dce72246b377426b83a52da1c84e6304b0e38
sunmd <sun-ming-da@163.com>
2020-10-21 18:35:05 +0800

    to(form)自动添加数据的表单

--- actualCombat/ant-design-vue-pro-test/src/views/Forms/BasicForm.vue
+++ actualCombat/ant-design-vue-pro-test/src/views/Forms/BasicForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-form :layout="formLayout">
+  <a-form :layout="formLayout" :form="form">
     <a-form-item
       label="Form Layout"
       :label-col="formItemLayout.labelCol"
@@ -24,21 +24,28 @@
       label="Field A"
       :label-col="formItemLayout.labelCol"
       :wrapper-col="formItemLayout.wrapperCol"
-      :validateStatus="fieldAStatus"
-      :help="fieldAHelp"
     >
-      <a-input v-model="fieldA" placeholder="input placeholder" />
+      <a-input
+        v-decorator="[
+          'fieldA',
+          {
+            initialvalue: '11',
+            rules: [{ required: true, min: 6, message: '必须大于6个字符' }]
+          }
+        ]"
+        placeholder="input placeholder"
+      />
     </a-form-item>
     <a-form-item
       label="Field B"
       :label-col="formItemLayout.labelCol"
       :wrapper-col="formItemLayout.wrapperCol"
     >
-      <a-input v-model="fieldB" placeholder="input placeholder" />
+      <a-input v-decorator="['fieldB']" placeholder="input placeholder" />
     </a-form-item>
     <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
       <a-button type="primary" @click="handleSubmit">
-        Submit
+        submit
       </a-button>
     </a-form-item>
   </a-form>
@@ -47,24 +54,17 @@
 <script>
 export default {
   data() {
+    this.form = this.$form.createForm(this);
     return {
-      formLayout: "horizontal",
-      fieldA: "",
-      fieldB: "",
-      fieldAStatus: "",
-      fieldAHelp: ""
+      formlayout: "horizontal",
+      fielda: "hello",
+      fieldb: ""
     };
   },
-  watch: {
-    fieldA(val) {
-      if (val.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "必须大于5个字符";
-      } else {
-        this.fieldAStatus = "";
-        this.fieldAHelp = "";
-      }
-    }
+  mounted() {
+    setTimeout(() => {
+      this.form.setFieldsValue({ filedA: "hello world!" });
+    }, 6000);
   },
   computed: {
     formItemLayout() {
@@ -90,15 +90,12 @@ export default {
       this.formLayout = e.target.value;
     },
     handleSubmit() {
-      if (this.fieldA.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "必须大于5个字符";
-      } else {
-        console.log({
-          fieldA: this.fieldA,
-          fieldB: this.fieldB
-        });
-      }
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values);
+          Object.assign(this, values);
+        }
+      });
     }
   }
 };
